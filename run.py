9dd375479089ec1eefda1d60a0840366684d1cb3
schottflo <florian.schottmann@gmx.de>
2022-09-07 14:11:30 +0200

    Add draft config implementation

--- run.py
+++ run.py
@@ -1,28 +1,34 @@
 from absl import app
 from absl import flags
+from ruamel.yaml import YAML
+from pathlib import Path
 
 from shmoo.core import api
 
 FLAGS = flags.FLAGS
-flags.DEFINE_multi_string("preprocessor", None, "Preprocessors.")
-flags.DEFINE_string("decoder", None, "Decoder.")
-flags.DEFINE_multi_string("predictor", None, "Predictor.")
-flags.DEFINE_multi_string("postprocessor", None, "Postprocessors.")
-flags.DEFINE_integer("bla", 1, "blub")
 
-flags.mark_flag_as_required("decoder")
+flags.DEFINE_string("config_path", None, "Path to a yaml file detailing model path, model framework, decoding method "
+                                         "and potential pre- or postprocessors.")
+flags.mark_flag_as_required("config_path")
 
 
 def main(argv):
     del argv  # Unused.
 
-    shmoo_decoder = api.Shmoo()
-    shmoo_decoder.set_up(preprocessor_specs=FLAGS.preprocessor,
-                         decoder_spec=FLAGS.decoder,
-                         predictor_specs=FLAGS.predictor,
-                         postprocessor_specs=FLAGS.postprocessor)
-    output_features = shmoo_decoder.decode_raw("Translate this sentence!")
-    print(output_features)
+    # Parse config file
+    yaml = YAML(typ='safe')
+    config = yaml.load(Path(FLAGS.config_path))
+
+    print(config)
+
+    # shmoo_decoder = api.Shmoo()
+    # shmoo_decoder.set_up(config=config)
+    # shmoo_decoder.set_up(preprocessor_specs=FLAGS.preprocessor,
+    #                      decoder_spec=FLAGS.decoder,
+    #                      predictor_specs=FLAGS.predictor,
+    #                      postprocessor_specs=FLAGS.postprocessor)
+    # output_features = shmoo_decoder.decode_raw("Translate this sentence!")
+    # print(output_features)
 
 
 if __name__ == "__main__":
--- shmoo/core/api.py
+++ shmoo/core/api.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Sequence, Tuple
+from typing import Any, Dict, Sequence, Tuple, Union
 
 from shmoo import prepostprocessing, decoders, predictors
 
@@ -18,26 +18,24 @@ class Shmoo:
         self._postprocessors = []
         self._decoder = None
 
-    def set_up(self, preprocessor_specs: Sequence[str], decoder_spec: str,
-               predictor_specs: Sequence[str],
-               postprocessor_specs: Sequence[str]) -> None:
-        for spec in preprocessor_specs:
-            name, config = _split_spec(spec)
-            self._preprocessors.append(
-                prepostprocessing.setup_processor(name, config))
+    def set_up(self, config: Dict[str, Any]) -> None:
 
-        decoder_name, decoder_config = _split_spec(decoder_spec)
-        self._decoder = decoders.setup_decoder(decoder_name, decoder_config)
+        # Assumption: There can only be one decoder
+        self._decoder = decoders.setup_decoder(config["decoder"].keys()[0], config)
 
-        for spec in predictor_specs:
-            name, config = _split_spec(spec)
-            self._decoder.add_predictor(
-                predictors.setup_predictor(name, config))
+        self._decoder.add_predictor(
+                predictors.setup_predictor(config["framework"], config)
+        )
 
-        for spec in postprocessor_specs:
-            name, config = _split_spec(spec)
-            self._postprocessors.append(
-                prepostprocessing.setup_processor(name, config))
+        # for spec in preprocessor_specs:
+        #     name, config = _split_spec(spec)
+        #     self._preprocessors.append(
+        #         prepostprocessing.setup_processor(name, config))
+
+        # for spec in postprocessor_specs:
+        #     name, config = _split_spec(spec)
+        #     self._postprocessors.append(
+        #         prepostprocessing.setup_processor(name, config))
 
     def decode_raw(self, raw: Any) -> Dict[str, Any]:
         return self.decode_features({"input_raw": raw})
